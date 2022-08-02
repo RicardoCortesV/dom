aae71f1335d45c0c76902956b4efd9c2f90074f8
RicardoCortesV <iudokinfo@gmail.com>
2022-08-01 22:50:27 -0500

    Uso de DOM y de Evento

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-sinStock = ["Alegrías, Pan, Dulces"]
 
-for (let i = 0; i < sinStock.length; i++) {
-    alert("Le informamos que tenemos poco stock de los siguientes artículos: " + sinStock[i]);
-} //Alerta de que hay poco stock de determinados arículos
 
 function Producto(nombre, stock, precio){  //USO CONSTRUCTOR DE OBJETOS. 
     this.nombre = nombre;
@@ -21,21 +17,7 @@ const item6 = new Producto('dulces', 30, 8);
 
 const carrito = [];
 
-function agregarAlCarrito(producto, stockDelProducto) { 
-    const tenemosStock = validarStock(stockDelProducto);    
-    if (tenemosStock === 'SÌ hay stock') {
-        console.log('Agregaste ' + ' unidades de ' + producto);     //Si el pedido es -10 unidades imprime el mensaje
-        carrito.push(producto);                      //Si el pedido es -10 unidades se agrega al array carrito  
-    } else {
-        alert('lo siento, no tenemos stock suficiente de ' + producto);
-    }
-    console.log("Esto se ha agregado al carrito: " + carrito);//Imprime 
-
-}
-
-
-
-function validarStock(stockDelProducto) {
+function validarStock(stockDelProducto) {//Indica la cantidad màxima de unidades a pedir
     if (stockDelProducto > 10) {
         return 'NO hay stock';
     } else {
@@ -43,13 +25,32 @@ function validarStock(stockDelProducto) {
     }
 }
 
+function agregarAlCarrito(producto, stockDelProducto) { //USO DE PUSH PARA AGREGAR AL ARRAY 
+    const tenemosStock = validarStock(stockDelProducto);    
+    if (tenemosStock === 'SÌ hay stock') {
+        console.log('Agregaste ' + ' unidades de ' + producto);     //Si el pedido es -10 unidades imprime el mensaje
+        carrito.push(producto);                      //Si el pedido es -10 unidades se agrega al array carrito  
+    } else {
+        alert('lo siento, no tenemos stock suficiente de ' + producto);
+    }
+
+}
+
+
+//USO DE DOM
 const lista = document.getElementById('lista')//Accede al elemento
 console.log(lista)
 
 const li = document.createElement('li')//Mètodo p/crear el texto
-li.textContent = 'Carrito de compras'//Crea un texto, esq sup der de la página
+li.textContent = 'Tus compras'//Crea un texto, esq sup der de la página
 
 lista.appendChild(li)
+
+let boton = document.getElementById("lista"); //USO DE EVENTO
+boton.addEventListener("click", respuestaClick);//Muestra lo que se ha pedido por medio de una alerta
+function respuestaClick(){
+    alert("Esto se ha agregado al carrito: " + carrito);
+}
 
 
 
